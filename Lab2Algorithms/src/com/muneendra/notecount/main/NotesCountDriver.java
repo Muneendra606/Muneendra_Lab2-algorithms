c1051f32b0b4e7c916cf8a96389fe251d243ba19
Muneendra606 <107765965+Muneendra606@users.noreply.github.com>
2022-07-11 20:56:56 +0530

    Add files via upload

---
+++ Lab2Algorithms/src/com/muneendra/notecount/main/NotesCountDriver.java
@@ -0,0 +1,30 @@
+package com.muneendra.notecount.main;
+
+import java.util.Scanner;
+
+import com.muneendra.notecount.service.MergeSortImplementation;
+import com.muneendra.notecount.service.NotesCount;
+
+public class NotesCountDriver {
+
+	public static void main(String[] args) {
+		
+		MergeSortImplementation mergesortimplementation= new MergeSortImplementation();
+		NotesCount notescount= new NotesCount();
+		System.out.println("enter the size of currency denominations:");
+		Scanner sc= new Scanner(System.in);
+		int size= sc.nextInt();
+		int[] notes= new int [size];
+		System.out.println(" enter the currency denominations value:");
+		 for (int i=0; i<size; i++)
+		 {
+			 notes[i]=sc.nextInt();
+		 }
+		 System.out.println("enter the amount you want to pay:");
+		 int amount= sc.nextInt();
+		 
+		 mergesortimplementation.sort(notes, 0 , notes.length-1);
+		 notescount.notesCountImplementation(notes, amount);
+	}
+
+}
